d4e7a9ba456021e0421ebd0f1ea99cd532fb78f7
hychan0517 <gjdbscks89@naver.com>
2020-11-18 14:16:10 +0900

    Create CameraResolutionController.cs

---
+++ CameraResolutionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraResolutionController : MonoBehaviour
+{
+	private void Awake()
+	{
+		Camera camera = GetComponent<Camera>();
+		Rect cameraRect = camera.rect;
+		float scaleHeight = (Screen.width / (float)Screen.height) / (16 / 10f);
+		float scaleWidth = 1f / scaleHeight;
+		if(scaleHeight < 1)
+		{
+			cameraRect.height = scaleHeight;
+			cameraRect.y = (1f - scaleHeight) / 2f;
+		}
+		else
+		{
+			cameraRect.width = scaleWidth;
+			cameraRect.x = (1f - scaleWidth) / 2f;
+		}
+		camera.rect = cameraRect;
+	}
+}
